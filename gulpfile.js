ee3fa84748cb751c7325fd7fb7cb51c283db2f29
Michele Gillis <gillismic@gmail.com>
2017-10-18 19:48:47 -0400

    add sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
 var gulp        = require('gulp');
+var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
 // Static server
-gulp.task('browser-sync', function() {
+gulp.task('serve', ['sass'], function() {
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
+    gulp.watch('./assets/sass/**/*.scss', ['sass'])
+    gulp.watch('./*.html').on('change', browserSync.reload);
+});
+gulp.task('sass', function() {
+    gulp.src('./assets/sass/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./assets/css'))
+    .pipe(browserSync.stream());
+});
+gulp.task('watch:sass', function() {
+    gulp.watch('./assets/sass/**/*.scss', ['sass'])
 });
